d81862a32f03465c2dde0b4a3817d2a40d42eef2
Subramanian-NG <subbu1150@gmail.com>
2024-07-24 12:58:18 -0400

    issue fix

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -5,7 +5,13 @@ export async function GET(request: NextRequest) {
     try
     {
     const categories = await getAllCategories();
-    return Response.json({ categories })
+    //return Response.json({ categories })
+    return new Response(JSON.stringify({ categories }), {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        },
+    });
     }
     catch(error)
     {
